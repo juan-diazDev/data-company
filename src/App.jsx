779da7602c1bb89423a0555487f5d24f5605e9d9
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-10-21 02:33:59 -0500

    feat: add Footer to view in all app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer';
 import SignupForm from './components/SignupForm';
 import Home from './pages/Home';
 
 const App = () => (
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/sign-up" element={<SignupForm />} />
-  </Routes>
+  <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-up" element={<SignupForm />} />
+    </Routes>
+    <Footer />
+  </>
 );
 
 export default App;
