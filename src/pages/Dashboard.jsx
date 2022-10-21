961b3e5e621079327c7a6887c565cb2be12098b8
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-10-21 02:45:54 -0500

    feat: initialize Dashboard page creation

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Dashboard/Header';
+
+const Dashboard = () => {
+  console.log('Dashboard');
+  return (
+    <main className="dashboard__main-container">
+      <Header />
+    </main>
+  );
+};
+
+export default Dashboard;
