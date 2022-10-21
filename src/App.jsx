d960092e8273322d25b13de84910ce1898f988f5
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-10-21 02:41:18 -0500

    feat: add dashboard to router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import SignupForm from './components/SignupForm';
+import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 
 const App = () => (
@@ -8,6 +9,7 @@ const App = () => (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sign-up" element={<SignupForm />} />
+      <Route path="/home/dashboard" element={<Dashboard />} />
     </Routes>
     <Footer />
   </>
