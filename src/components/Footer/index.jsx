602ab6d15453837ed6272c3331e4b947d4ae3a93
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-10-21 02:31:30 -0500

    feat: add footer to components folder

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,21 @@
+import './styles.scss';
+
+const Footer = () => (
+  <footer className="footer__main-container">
+    <section className="footer__copyright-wraper">
+      <p>
+        Copyright © Juan-diazDev
+      </p>
+      <p>
+        Oct, 2022
+      </p>
+    </section>
+
+    <section className="footer__my-info-wraper">
+      <a href="http://linkedin.com/in/juan-d iazdev" target="_blanck">LinkedIN</a>
+      <a href="http://github.com/juan-diazDev" target="_blanck">My Github</a>
+    </section>
+  </footer>
+);
+
+export default Footer;
